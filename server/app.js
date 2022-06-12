a5140d2663bc0334d0b842c7bfbfe3d8f78c0da5
abdolrhman <iwcw4i@gmail.com>
2022-06-12 12:24:53 +0200

    init commit

---
+++ server/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const {Agent} = require('./model');
+
+const app = express();
+app.use(express.json())
+
+app.get('/agents', async (req, res, next) => {
+  const agents = await Agent.findAll();
+  return res.json(agents);
+});
+
+module.exports = app;
